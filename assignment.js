2f566cf9131ae6a86cf9808e583a8fe27e74b38f
DonnyAriyan <113486897+DonnyAriyan@users.noreply.github.com>
2022-10-17 21:22:03 +1100

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,35 @@
+var vg_1 = "assignment.json";
+vegaEmbed("#choropleth", vg_1).then(function(result) {
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "assignmentBubble.json";
+vegaEmbed("#bubblechart", vg_2).then(function(result) {
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "assignmentlinechartWorld.json";
+vegaEmbed("#line", vg_3).then(function(result) {
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_4 = "assignmentheatmap.json";
+vegaEmbed("#heatmap", vg_4).then(function(result) {
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_5 = "assignmentheatmap2.json";
+vegaEmbed("#heatmap2", vg_5).then(function(result) {
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_6 = "assignmentbarchartadj.json";
+vegaEmbed("#barchart", vg_6).then(function(result) {
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
